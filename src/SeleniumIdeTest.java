c4393f1b7bf5c4363cefb4f19b4d405b5bb32e63
Esteban <iebvidea@gmail.com>
2015-07-29 22:43:25 -0500

    VisibilityConditions

--- src/SeleniumIdeTest.java
+++ src/SeleniumIdeTest.java
@@ -1,4 +1,3 @@
-
 import java.util.concurrent.TimeUnit;
 import org.junit.*;
 import static org.junit.Assert.*;
@@ -6,45 +5,44 @@ import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class SeleniumIdeTest {
-  private WebDriver driver;
-  private String baseUrl;
-  private StringBuffer verificationErrors = new StringBuffer();
+	private WebDriver driver;
+	private String baseUrl;
+	private StringBuffer verificationErrors = new StringBuffer();
 
-  @Before
-  public void setUp() throws Exception {
-    driver = new FirefoxDriver();
-    baseUrl = "https://accounts.google.com/";
-    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-  }
+	@Before
+	public void setUp() throws Exception {
+		driver = new FirefoxDriver();
+		baseUrl = "https://accounts.google.com/";
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	}
 
-  @Test
-  public void test() throws Exception {
-    driver.get(baseUrl);
-    driver.findElement(By.id("Email")).clear();
-    driver.findElement(By.id("Email")).sendKeys("iebvidea");
-    driver.findElement(By.id("next")).click();
-    driver.findElement(By.id("Passwd")).clear();
-    driver.findElement(By.id("Passwd")).sendKeys("91060409442");
-    driver.findElement(By.id("signIn")).click();
-    driver.findElement(By.cssSelector("span.gb_d.gbii")).click();
-    driver.findElement(By.id("gb_71")).click();
-  }
+	@Test
+	public void test() throws Exception {
+		driver.get(baseUrl);
+		driver.findElement(By.id("Email")).clear();
+		driver.findElement(By.id("Email")).sendKeys("iebvidea");
+		driver.findElement(By.id("next")).click();
+		driver.findElement(By.id("Passwd")).clear();
+		driver.findElement(By.id("Passwd")).sendKeys("91060409442");
+		driver.findElement(By.id("signIn")).click();
+		driver.findElement(By.cssSelector("span.gb_d.gbii")).click();
+		driver.findElement(By.id("gb_71")).click();
+	}
 
-  @After
-  public void tearDown() throws Exception {
-	driver.close();
-	Thread.sleep(1000);
-    //driver.quit();
-	//This is added instead to avoid the firefox 30 bug
-	Runtime.getRuntime().exec("taskkill /F /IM firefox.exe");
-	Thread.sleep(5000);
-	Runtime.getRuntime().exec("taskkill /F /IM plugin-container.exe");
-	Runtime.getRuntime().exec("taskkill /F /IM WerFault.exe");
-    String verificationErrorString = verificationErrors.toString();
-    if (!"".equals(verificationErrorString)) {
-      fail(verificationErrorString);
-    }
-  }
-
+	@After
+	public void tearDown() throws Exception {
+		driver.close();
+		Thread.sleep(1000);
+		// driver.quit();
+		// This is added instead to avoid the firefox 30 bug
+		Runtime.getRuntime().exec("taskkill /F /IM firefox.exe");
+		Thread.sleep(5000);
+		Runtime.getRuntime().exec("taskkill /F /IM plugin-container.exe");
+		Runtime.getRuntime().exec("taskkill /F /IM WerFault.exe");
+		String verificationErrorString = verificationErrors.toString();
+		if (!"".equals(verificationErrorString)) {
+			fail(verificationErrorString);
+		}
+	}
 
 }
